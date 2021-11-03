5147437a859a94d67a9d4267845f9b5b1eb6e25c
brianmuganda <brian.ashiali@student.moringaschool.com>
2021-11-03 09:33:54 +0300

    add my home component and edit it

---
+++ src/app/components/my-home/my-home.component.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Repository } from 'src/app/models/repository';
+import { User } from 'src/app/models/user';
+import { ServicesService } from 'src/app/services/services.service';
+
+@Component({
+  selector: 'app-my-home',
+  templateUrl: './my-home.component.html',
+  styleUrls: ['./my-home.component.css']
+})
+export class MyHomeComponent implements OnInit {
+
+  user:User;
+  repos:Repository[];
+
+  showRepositories=false;
+
+  toggleRepositories(){
+    this.showRepositories=!this.showRepositories
+  }
+
+  
+
+  constructor(private servicesService: ServicesService, private http:HttpClient) { }
+
+  ngOnInit(){
+    this.servicesService.requestUserInfo("brianmuganda")
+    this.user = this.servicesService.user
+    this.servicesService.requestRepositories("brianmuganda")
+    this.repos = this.servicesService.repos
+
+    this.showRepositories=false;
+  }
+
+}
